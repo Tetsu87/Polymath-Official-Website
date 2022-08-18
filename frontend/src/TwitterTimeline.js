5c3555c0b863f116cc9bbc1eae69d573dcacda80
Tkai87 <tkai@usc.edu>
2022-08-18 01:19:20 -0700

    twitter timeline re-created with twitter-publish

--- frontend/src/TwitterTimeline.js
+++ frontend/src/TwitterTimeline.js
@@ -1,23 +1,37 @@
 import React from "react";
 import "./TwitterTimeline.css";
 import { Timeline } from "react-twitter-widgets";
+import { useCallback, useEffect, useState } from "react";
 
 function TwitterTimeline() {
+  useEffect(() => {
+    // read script
+    const script = document.createElement("script");
+    script.src = "https://platform.twitter.com/widgets.js";
+    document.body.appendChild(script);
+    // delete script tag for unmount
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div className="timeline_wrapper mt-3 mx-3">
+    <div className="timeline-wrapper mt-3 ">
       <div>
-        <main>
-          <Timeline className="timeline-height"
-            dataSource={{
-              sourceType: "profile",
-              screenName: "PolymathNYC",
-            }}
-            options={{
-              height: "500",
-              width:"375",
-            }}
-          />
-        </main>
+        <a
+          class="twitter-timeline"
+          data-width="375"
+          data-height="600"
+          data-theme="light"
+          href="https://twitter.com/PolymathNYC?ref_src=twsrc%5Etfw"
+        >
+          Tweets by PolymathNYC
+        </a>{" "}
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        ></script>
       </div>
     </div>
   );
