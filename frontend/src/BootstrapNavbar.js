08a95b9e20555ad62109963456c3a8eb74db0b13
Tkai87 <tkai@usc.edu>
2022-08-18 18:12:17 -0700

    re-created navbar sp ver

--- frontend/src/BootstrapNavbar.js
+++ frontend/src/BootstrapNavbar.js
@@ -9,9 +9,46 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import MediaQuery from "react-responsive";
 
 function BootstrapNavbar() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
-      <MediaQuery query="(max-width: 767px)"></MediaQuery>
+      <MediaQuery query="(max-width: 767px)">
+        <Button variant="primary" onClick={handleShow}>
+          Launch
+        </Button>
+
+        <Offcanvas show={show} onHide={handleClose} placement="end">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav
+              className="justify-content-around flex-grow-1 pe-3"
+              onClick={handleClose}
+            >
+              <Nav.Link href="#home">Home</Nav.Link>
+              <a type="button" href="#value">
+                aaa
+              </a>
+              <Nav.Link href="#team">Team</Nav.Link>
+              <Nav.Link href="#value">Value</Nav.Link>
+              <Nav.Link href="#achievements">Achievements</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="#social">Social</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </MediaQuery>
+
+
+
+
+
+
+      
       <MediaQuery query="(min-width: 768px)">
         <div className="navbar-pc-wrapper py-3">
           <div className="navbar-width px-3 pt-3">
@@ -20,22 +57,37 @@ function BootstrapNavbar() {
                 <img src={polymath_logo} className="polymath-logo" />
               </div>
               <div>
-                <a className="nav-link-decoration navbar-color" href="#home">Home</a>
+                <a className="nav-link-decoration navbar-color" href="#home">
+                  Home
+                </a>
               </div>
               <div>
-                <a className="nav-link-decoration navbar-color" href="#team">Team</a>
+                <a className="nav-link-decoration navbar-color" href="#team">
+                  Team
+                </a>
               </div>
               <div>
-                <a className="nav-link-decoration navbar-color" href="#value">Value</a>
+                <a className="nav-link-decoration navbar-color" href="#value">
+                  Value
+                </a>
               </div>
               <div>
-                <a className="nav-link-decoration navbar-color" href="#achievements">Achievements</a>
+                <a
+                  className="nav-link-decoration navbar-color"
+                  href="#achievements"
+                >
+                  Achievements
+                </a>
               </div>
               <div>
-                <a className="nav-link-decoration navbar-color" href="#contact">Contact</a>
+                <a className="nav-link-decoration navbar-color" href="#contact">
+                  Contact
+                </a>
               </div>
               <div>
-                <a className="nav-link-decoration navbar-color" href="#social">Social</a>
+                <a className="nav-link-decoration navbar-color" href="#social">
+                  Social
+                </a>
               </div>
             </div>
           </div>
