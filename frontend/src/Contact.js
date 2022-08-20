d41d51b8ce743d06d6fd82539b44efd6671b368a
Tkai87 <tkai@usc.edu>
2022-08-20 13:33:26 -0700

    line button created

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Contact.css";
 import line from "./images/line.png";
+import line_svg from "./images/line-messenger.svg";
+import line_base from "./images/line_login_base.png";
 import MediaQuery from "react-responsive";
 import Button from "react-bootstrap/Button";
 
@@ -121,139 +123,106 @@ function Contact() {
             簡単なご相談はLINEでも承っております。お気軽にどうぞ。
           </p>
 
-          <style type="text/css">
-            {`
-    .btn-line {
-      background-color: #09b53b;
-      color: white;
-    }
-
-    .btn-xxl {
-      padding: 0.5rem 1.2rem;
-      font-size: 1.2rem;
-    }
-    `}
-          </style>
-
           <a href="https://lin.ee/Rz2NZubS" target="_blank">
-            <div className="line-wrapper">
-              <div className="px-3">
-                <img src={line} className="line-logo" />
+              <div className="line-wrapper">
+                <div className="line-relative">
+                  <img src={line_base} className="line-logo" />
+                  <div className="line-absolute">LINEで相談</div>
+                </div>
               </div>
-              <div>
-                <Button variant="line" size="xxl">
-                  LINEで相談する
-                </Button>
-              </div>
-            </div>
-          </a>
+            </a>
         </MediaQuery>
 
         <MediaQuery query="(min-width: 768px)">
-          <p className="text-center small-letter pc">
-            無料相談の受付を随時承ってます。お気軽にご連絡ください。
-          </p>
-          <div className="mb-1">
-            <input
-              type="text"
-              id="name"
-              placeholder="名前"
-              className="pc"
-              required
-            />
-          </div>
-          <div className="mb-1">
-            <input
-              type="email"
-              id="email"
-              placeholder="メールアドレス"
-              className="pc"
-              required
-            />
-          </div>
-          <div className="mb-1">
-            <input
-              type="text"
-              id="tel"
-              placeholder="電話番号"
-              className="pc"
-              required
-            />
-          </div>
-          <div className="mb-1">
-            <textarea
-              id="message"
-              placeholder="問い合わせ内容"
-              className="pc"
-              required
-            ></textarea>
-          </div>
-          <div>
-            <select required id="options" className="mt-3 option-font pc">
-              <option required selected value="">
-                {" "}
-                当サービスを知ったきっかけ*
-              </option>
-              <option required value="Twitter">
-                Twitter
-              </option>
-              <option required value="Facebook">
-                Facebook
-              </option>
-              <option required value="Youtube">
-                Youtube
-              </option>
-              <option required value="Note">
-                Note
-              </option>
-              <option required value="友人">
-                友人
-              </option>
-              <option required value="会社">
-                会社
-              </option>
-              <option required value="大学">
-                大学
-              </option>
-              <option required value="その他">
-                その他
-              </option>
-            </select>
-          </div>
-          <button type="submit" className="mt-3 mb-3 button">
-            {status}
-          </button>
-
-          <p className="text-center small-letter pc">
-            簡単なご相談はLINEでも承っております。お気軽にどうぞ。
-          </p>
-
-          <style type="text/css">
-            {`
-    .btn-line {
-      background-color: #09b53b;
-      color: white;
-    }
-
-    .btn-xxl {
-      padding: 0.5rem 1.2rem;
-      font-size: 1.2rem;
-    }
-    `}
-          </style>
-
-          <a href="https://lin.ee/Rz2NZubS" target="_blank" className="line-button">
-            <div className="line-wrapper">
-              <div className="px-3">
-                <img src={line} className="line-logo" />
-              </div>
-              <div>
-                <Button variant="line" size="xxl">
-                  LINEで相談する
-                </Button>
-              </div>
+          <div className="contact-wrapper-pc">
+            <p className="text-center small-letter pc">
+              無料相談の受付を随時承ってます。お気軽にご連絡ください。
+            </p>
+            <div className="mb-1">
+              <input
+                type="text"
+                id="name"
+                placeholder="名前"
+                className="pc"
+                required
+              />
             </div>
-          </a>
+            <div className="mb-1">
+              <input
+                type="email"
+                id="email"
+                placeholder="メールアドレス"
+                className="pc"
+                required
+              />
+            </div>
+            <div className="mb-1">
+              <input
+                type="text"
+                id="tel"
+                placeholder="電話番号"
+                className="pc"
+                required
+              />
+            </div>
+            <div className="mb-1">
+              <textarea
+                id="message"
+                placeholder="問い合わせ内容"
+                className="pc"
+                required
+              ></textarea>
+            </div>
+            <div>
+              <select required id="options" className="mt-3 option-font pc">
+                <option required selected value="">
+                  {" "}
+                  当サービスを知ったきっかけ*
+                </option>
+                <option required value="Twitter">
+                  Twitter
+                </option>
+                <option required value="Facebook">
+                  Facebook
+                </option>
+                <option required value="Youtube">
+                  Youtube
+                </option>
+                <option required value="Note">
+                  Note
+                </option>
+                <option required value="友人">
+                  友人
+                </option>
+                <option required value="会社">
+                  会社
+                </option>
+                <option required value="大学">
+                  大学
+                </option>
+                <option required value="その他">
+                  その他
+                </option>
+              </select>
+            </div>
+            <button type="submit" className="mt-3 mb-3 button">
+              {status}
+            </button>
+          </div>
+
+          <div>
+            <p className="text-center small-letter pc">
+              簡単なご相談やご質問はLINEでも承っております。お気軽にどうぞ。
+            </p>
+            <a href="https://lin.ee/Rz2NZubS" target="_blank">
+              <div className="line-wrapper">
+                <div className="line-relative">
+                  <img src={line_base} className="line-logo" />
+                  <div className="line-absolute">LINEで相談</div>
+                </div>
+              </div>
+            </a>
+          </div>
         </MediaQuery>
       </div>
     </form>
